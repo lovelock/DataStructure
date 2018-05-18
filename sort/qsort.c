8b3951ab9c06eeb2e6ea7c4ab900fcd0513c4113
Frost Wong <frostwong@gmail.com>
2018-05-18 23:23:30 +0800

    var_export lol

--- sort/qsort.c
+++ sort/qsort.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
-#include "qsort.h"
+
+#define ARRAY_LEN 6
+
+void var_export(int *, int);
+
+void qsort(int *, int, int);
+
+int main()
+{
+    int array[ARRAY_LEN] = {3, 9, 2, 1, 0, 43};
+
+    var_export(array, ARRAY_LEN);
+
+    qsort(array, 0, ARRAY_LEN - 1);
+
+    var_export(array, ARRAY_LEN);
+
+    return 0;
+}
 
 
-void qsort(int *array, int low, int high) {
+void qsort(int * array, int low, int high) {
+
     if (low >= high) {
         return;
     }
 
     int first = low;
     int last = high;
-    int key = array[first];
+    int pivot = array[first];
 
     while (first < last) {
-        while (first < last && array[last] > key) {
-            last--;
+        while (first < last && array[last] > pivot) {
+            --last;
         }
 
         if (first < last) {
             array[first] = array[last];
         }
 
-        while (first < last && array[first] < key) {
-            first++;
+        while (first < last && array[first] < pivot) {
+            ++first;
         }
 
         if (first < last) {
@@ -29,8 +48,21 @@ void qsort(int *array, int low, int high) {
         }
     }
 
-    array[first] = key;
+    array[first] = pivot; // equals array[last] = pivot; because last = first
 
-    qsort(array, low, first -1);
+    qsort(array, low, first - 1);
     qsort(array, first + 1, high);
+
+}
+
+void var_export(int * array, int len) {
+    printf("\n[\n");
+
+    int i = 0;
+
+    for(;i < len; i++) {
+        printf("\t%d,\n", array[i]);
+    }
+
+    printf("]\n");
 }
