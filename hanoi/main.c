eb74804314a2916de03a41a7a6f35ada7cbbc1ac
Frost Wong <frostwong@gmail.com>
2018-05-24 22:39:04 +0800

    format with clang

--- hanoi/main.c
+++ hanoi/main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-void hanoi(int n, char A, char B, char C) {
-    if (n == 1) {
+void hanoi(int n, char A, char B, char C)
+{
+    if (n == 1)
+    {
         printf("%d: %c => %c\n", n, A, C);
-    } else {
-        hanoi(n-1, A, C, B);
+    }
+    else
+    {
+        hanoi(n - 1, A, C, B);
         printf("%d: %c => %c\n", n, A, C);
-        hanoi(n-1,B, A, C);
+        hanoi(n - 1, B, A, C);
     }
 }
 
-
-int main() {
+int main()
+{
     char A = 'A';
     char B = 'B';
     char C = 'C';
