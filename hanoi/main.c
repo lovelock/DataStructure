1835142a730706d7614062f158e0876c56d15684
Frost Wong <frostwong@gmail.com>
2018-05-24 22:33:00 +0800

    hanoi

---
+++ hanoi/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void hanoi(int n, char A, char B, char C) {
+    if (n == 1) {
+        printf("1: %c => %c\n", A, C);
+    } else {
+        hanoi(n-1, A, C, B);
+        printf("%d: %c => %c\n", n, A, C);
+        hanoi(n-1,B, A, C);
+    }
+}
+
+
+int main() {
+    char A = 'A';
+    char B = 'B';
+    char C = 'C';
+
+    int n;
+
+    printf("please input dishes to move\n");
+    scanf("%d", &n);
+
+    hanoi(n, A, B, C);
+}
